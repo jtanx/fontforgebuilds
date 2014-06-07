d771f55fecf221392872c36520fc7ad88fa5a5d5
Jeremy Tan <jtanx@users.noreply.github.com>
2014-06-07 13:36:09 +0800

    Add custom MSYS2 configs to make life easier.
    Usage: ./patch-msys2-config.sh
    What it does:
    * You can use Ctrl+Left/Right to navigate forwards/backwards one
    word.
    * If installed, Notepad++ is set as the default editor of git.
      Also, a short-cut is added, so you can call Notepad++ by
      simply typing `npp`.
    
      If you have installed Notepad++ somewhere non-standard, then
      this won't work.

---
+++ patch-msys2-config.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/sh
+
+# Yellow text
+function log_status() {
+    echo -e "\e[33m$@\e[0m"
+}
+
+irc=$(cat inputrc-config.txt)
+if ! grep -Fxq "$irc" ~/.inputrc; then
+	log_status "Patching .inputrc..."
+	cat inputrc-config.txt >> ~/.inputrc
+	bind -f ~/.inputrc
+fi
+
+npp="C:/Program Files (x86)/Notepad++/notepad++.exe"
+if [ ! -f "$npp" ]; then
+	npp="C:/Program Files/Notepad++/notepad++.exe"
+	if [ ! -f "$npp" ]; then
+		npp=""
+	fi;
+fi
+
+# We have Notepad++
+if [ ! -z "$npp" ]; then
+	nppex="alias npp=\"\\\"$npp\\\"\""
+	if ! grep -Fxq "$nppex" ~/.bash_profile; then
+		log_status "Adding npp shortcut to ~/.bash_profile..."
+		printf "\n%s\n" "$nppex" >> ~/.bash_profile
+	fi;
+	
+	log_status "Using notepad++ as git editor..."
+	git config --global core.editor "'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin"
+fi
+
+log_status "Done."
