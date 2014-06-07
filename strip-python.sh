0a8b1070408cf888d0db866e2ba46c9d6dd470b9
Jeremy Tan <jtanx@users.noreply.github.com>
2014-06-07 17:24:51 +0800

    32/64-bit selection

--- strip-python.sh
+++ strip-python.sh
@@ -2,7 +2,11 @@
 # Copies the Python Lib directory and strips it down to a reasonable size.
 # FontForge essentially ships with its own version of Python
 
-PYVER=python3.4
+if [ "$MSYSTEM" = "MINGW32" ]; then
+	PYVER=python2.7
+else
+	PYVER=python3.4
+fi
 PYDIR=/$MSYSTEM/lib/$PYVER
 
 cd original-archives/binaries
@@ -12,7 +16,8 @@ if [ ! -d $PYVER ]; then
 fi
 
 cd $PYVER
-rm -rfv config-3.4m idlelib test turtledemo 
+rm -rfv config-3.4m idlelib test turtledemo config
+find . -regextype sed -regex ".*\.py[co]" | xargs rm -rfv
 find . -name __pycache__ | xargs rm -rfv
 find . -name test | xargs rm -rfv
 find . -name tests | xargs rm -rfv
