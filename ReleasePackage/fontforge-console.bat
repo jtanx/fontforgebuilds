9dcd52fb2ebd67d05dee7c677b39facb1c9b7128
Jeremy Tan <jtanx@users.noreply.github.com>
2014-06-16 20:33:53 +0800

    Make ffpython callable

--- ReleasePackage/fontforge-console.bat
+++ ReleasePackage/fontforge-console.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Configuring the system path to add FontForge...
 set FF=%~dp0
-set PATH=%FF%;%PATH%
+set PATH=%FF%;%FF%\bin;%PATH%
 
 echo Configuration complete. You can now call 'fontforge' from the console.
-echo You may also use the bundled Python distribution by calling `ffpython.exe`.
-echo Extra Python modules that you require may be installed via `ffpython.exe`.
+echo You may also use the bundled Python distribution by calling `ffpython`.
+echo Extra Python modules that you require may be installed via `ffpython`.
 echo.
 cmd /k
