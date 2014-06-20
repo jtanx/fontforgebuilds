ae9e7946a040a871c58f44b3af34af6920bdc3e5
Jeremy Tan <jtanx@users.noreply.github.com>
2014-06-20 18:18:56 +0800

    Update the setup one too

--- fontforge-setup/fontforge.bat
+++ fontforge-setup/fontforge.bat
@@ -1,9 +1,19 @@
 @ECHO OFF
 set FF=%~dp0
-set PATH=%FF%\bin;%FF%\bin\VcXsrv;%PATH%
 set DISPLAY=127.0.0.1:9.0
 set XLOCALEDIR=%FF%\bin\VcXsrv\locale
 set AUTOTRACE=potrace
+set HOME=%FF%
+::Set this to your language code to change the FontForge UI language
+::See share/locale/ for a list of supported language codes
+::set LANGUAGE=en
+
+
+::Only add to the path if it's not already there
+set FFPATH=%FF%\bin
+call "%FF%\bin\AddToPath" FFPATH /B || goto PathFail
+set FFPATH=%FF%
+call "%FF%\bin\AddToPath" FFPATH /B || goto PathFail
 
 "%FF%\bin\VcXsrv_util.exe" -exists || (
 start /B "" "%FF%\bin\VcXsrv\vcxsrv.exe" :9 -multiwindow -clipboard -silent-dup-error
@@ -13,4 +23,12 @@ start /B "" "%FF%\bin\VcXsrv\vcxsrv.exe" :9 -multiwindow -clipboard -silent-dup-
 
 "%FF%\bin\fontforge.exe" -nosplash %*
 
+goto ok
+
+:PathFail
+echo Failed to set the PATH variable
+
+:ok
 "%FF%\bin\VcXsrv_util.exe" -close
+:: bye
+
