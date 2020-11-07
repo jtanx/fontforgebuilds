a31a88f3e3a3fc92c42e70cb93a5c25404834248
Jeremy Tan <jtanx@outlook.com>
2020-11-07 21:14:51 +1100

    Make python libs from the pacman archive

--- strip-python.sh
+++ strip-python.sh
@@ -2,27 +2,30 @@
 # Copies the Python Lib directory and strips it down to a reasonable size.
 # FontForge essentially ships with its own version of Python
 
+set -eo pipefail
 
-PYVER="$1"
-PYDIR=/$MSYSTEM/lib/$PYVER
+export LC_ALL=C
 
-if [ -z "$PYVER" ]; then
-    echo "Usage: $0 python-version"
-    exit 1
+if [ "$MSYSTEM" == "MINGW32" ]; then
+    PACKAGE=mingw-w64-i686-python3
+else
+    PACKAGE=mingw-w64-x86_64-python3
 fi
+
+PYNAME=$(pacman -Qi $PACKAGE | grep -m1 Name | cut -d':' -f2 | xargs)
+PYFULLVER=$(pacman -Qi $PACKAGE | grep -m1 Version | cut -d':' -f2 | xargs)
+PYVER=python$(echo $PYFULLVER | cut -d. -f1-2)
+PYARCH=$(ls -1 "/var/cache/pacman/pkg/$PYNAME-$PYFULLVER-any.pkg.tar."* | head -n1)
 
 cd original-archives/binaries || exit 1
 
+echo "Python: $PYNAME $PYFULLVER ($PYVER): $PYARCH"
+
 if [ ! -d $PYVER ]; then
-	cp -r $PYDIR . || exit 1
+    tar axf "/var/cache/pacman/pkg/$PYNAME-$PYFULLVER-any.pkg.tar."* --strip-components=2 --wildcards '*/lib/python3.*'
 fi
 
 cd $PYVER
-rm -rfv config-3.4m idlelib test turtledemo config
+rm -rfv "config-${PYVER}" idlelib test turtledemo config
 find . -regextype sed -regex ".*\.py[co]" | xargs rm -rfv
 find . -name __pycache__ | xargs rm -rfv
-find . -name test | xargs rm -rfv
-find . -name tests | xargs rm -rfv
-cd ..
-
-cd ../..
