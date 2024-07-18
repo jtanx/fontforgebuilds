51b7c69c4fce70b795af6f41408b4d61290fb216
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-07-18 08:51:08 +0300

    Add MinGW gtkmm dependency

--- ffbuild.sh
+++ ffbuild.sh
@@ -280,7 +280,7 @@ if (( ! $nomake )) && [ ! -f $PMTEST ]; then
     if (($qt)); then
         pacman $IOPTS $PMPREFIX-{qt6,qt6-tools}
     else
-        pacman $IOPTS $PMPREFIX-gtk3
+        pacman $IOPTS $PMPREFIX-{gtk3,gtkmm3}
     fi
 
     touch $PMTEST
